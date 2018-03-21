5e312da1e9b4ed0c331d74def11037181b6f8986
alu53794939d <alu53794939d@ti2-02>
2018-03-21 19:52:39 +0100

    conflict totally solved

--- UnitFiveWork/src/WorkshopApp.java
+++ UnitFiveWork/src/WorkshopApp.java
@@ -1,27 +1,8 @@
 import java.util.Date;
 
 public class WorkshopApp {
-	
-
 	private Product product1 = new Product();
-	//Client info
-	private int codCli;
-	private String cliDNI;
-	private String cliName;
-	private String cliFirstName;
-	
-	
-	
-	//Invoice info
-	private int codInvoice;
-	private Date dateInvoice;
-	private double sum;
-	private int discount;
-	
-	
-
-	private Product data = new Product();
-	private Client Client1 = new Client();
+	private Client client1 = new Client();
 	private Invoice invoice1 = new Invoice();
 
 	//Invoice line info
